afa02e299cb317353f3271fdd213e9551cee1376
Sigurborg <sigselma@gmail.com>
2022-09-15 19:20:12 +0000

    finish

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Module3G7
+For this assignment I reallhy wanted to try some new things. 
+I did Event Listeners button witch was fun. I did a Button witch changes colors.
+
+Then I started trying out making a tic tac toe game. That became very complicated and a huges challange for me. Javascript is not my strong game but I am learning :)
--- main.js
+++ main.js
@@ -1,12 +1,86 @@
-$(document).ready(function() {
+const cells = document.querySelectorAll (".cell");
+const statusText =document.querySelector("#statusText");
+const restartBtn =document.querySelector("#restartBtn");
+const winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+let options =["","","","","","","","","",];
+let currentPlayer = "X";
+let running = false;
 
-    function goLeft() {
-        $("#bug").animate({left: "-=300"}, 1500, "swing", goRight);
+initializeGame();
+
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click",cellClicked));
+    restartBtn.addEventListener("click",restartGame);
+    statusText.textContent = currentPlayer + 's turn';
+    running = true;
+}
+function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex")
+
+
+    if(options[cellIndex] != "" || !running){
+        return;
     }
+
+    UpdateCell(this,cellIndex)
+    checkWinner ();
+
+}
+function UpdateCell(cell, index){
+    options[index] = currentPlayer;
+    cell.textContent=currentPlayer;
+
+}//if current player is equal to x we use O otherwise
+function changePlayer(){
+    currentPlayer = (currentPlayer=="X") ? "O" : "X";
+    statusText.textContent = currentPlayer + 's turn'; 
+}
+function checkWinner(){
+    let roundWon = false
     
-    function runningRight() {
-        $("#bug").animate({left: "+=300"}, 1500, "swing", goLeft);
+    for(let i=0; i<winConditions.length; i++){
+        const conditions = winConditions[i];
+        const cellA = options[conditions[0]];
+        const cellB = options[conditions[1]];
+        const cellC = options[conditions[2]];
+
+        if(cellA == "" || cellB =="" || cellC == "") {
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
     }
-    
-    goRight();});
-    
+    if(roundWon){
+        statusText.textContent = currentPlayer+ 'wins!';
+        running = false;
+
+    }
+   
+    else if(!options.includes("")){
+        statusText.textContent = 'Draw!';
+        running= false;
+
+    }
+    else{
+        changePlayer();
+    }
+}
+function restartGame(){
+    currentPlayer = "X";
+    options = ["","","","","","","","",""]
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach (cell => cell.textContent = "");
+    running = true
+
+}
